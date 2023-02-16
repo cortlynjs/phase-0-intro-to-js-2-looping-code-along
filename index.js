61ddba6097ed511119dc44f8b559ec51369aa622
Cortlyn Stafford <corltynjimmel@aol.com>
2023-02-16 03:15:27 -0500

    looping lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+  let thankYouMessages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+
+  return thankYouMessages;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
